e6a0fabd0d235c828a3e40ec11121f04c2ae695c
Izuchukwu Ogbodo <matthiasogbodo@gmail.com>
2019-04-06 15:10:53 +0100

    Empty arguments

--- Division/Division.test.js
+++ Division/Division.test.js
@@ -3,3 +3,6 @@ const divide = require("./Division");
 test("divide 4 / 4 to equals 1", () => {
   expect(divide(4, 4)).toBe(1);
 });
+test("Empty arguments ", () => {
+  expect(divide()).toEqual("No parameter should be empty");
+});
