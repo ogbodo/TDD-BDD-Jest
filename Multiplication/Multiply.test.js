ae6fcd509293678be93c16e2e9e78758a099d4e1
Izuchukwu Ogbodo <matthiasogbodo@gmail.com>
2019-04-06 14:07:49 +0100

    One argument not number

--- Multiplication/Multiply.test.js
+++ Multiplication/Multiply.test.js
@@ -6,3 +6,9 @@ test("Multiply 2 * 4 to equals 8", () => {
 test("Empty arguments ", () => {
   expect(multiply()).toEqual("No parameter should be empty");
 });
+test("One Empty argument ", () => {
+  expect(multiply(2)).toBe("No parameter should be empty");
+});
+test("One argument not number", () => {
+  expect(multiply([], 2)).toBe("All Parameters should be number");
+});
