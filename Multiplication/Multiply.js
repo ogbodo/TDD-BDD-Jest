084f5fa4143ab098f9ffe6dfb964c89b95fcd6c2
Izuchukwu Ogbodo <matthiasogbodo@gmail.com>
2019-04-06 14:11:57 +0100

    Passed test => One argument not number

--- Multiplication/Multiply.js
+++ Multiplication/Multiply.js
@@ -2,5 +2,8 @@ module.exports = function(a, b) {
   if (!(a && b)) {
     return "No parameter should be empty";
   }
+  if (typeof a !== "number" || typeof b !== "number") {
+    return "All Parameters should be number";
+  }
   return a * b;
 };
