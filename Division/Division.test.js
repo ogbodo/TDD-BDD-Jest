426e5267c3490d90f85ed30c2d2ab5aaf2706f40
Izuchukwu Ogbodo <matthiasogbodo@gmail.com>
2019-04-06 15:16:07 +0100

    One argument not number

--- Division/Division.test.js
+++ Division/Division.test.js
@@ -6,3 +6,9 @@ test("divide 4 / 4 to equals 1", () => {
 test("Empty arguments ", () => {
   expect(divide()).toEqual("No parameter should be empty");
 });
+test("One Empty argument ", () => {
+  expect(divide(2)).toBe("No parameter should be empty");
+});
+test("One argument not number", () => {
+  expect(divide([], 2)).toBe("All Parameters should be number");
+});
