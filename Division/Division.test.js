1da061931e67cb65ae8f21af3d5e397842eca6ac
Izuchukwu Ogbodo <matthiasogbodo@gmail.com>
2019-04-06 17:12:13 +0100

    the case of null arguments

--- Division/Division.test.js
+++ Division/Division.test.js
@@ -7,7 +7,7 @@ test("Empty arguments ", () => {
   expect(divide()).toEqual("No parameter should be empty");
 });
 test("One Empty argument ", () => {
-  expect(divide(2)).toBe("Second parameter should not be empty");
+  expect(divide(2)).toBe("All Parameters should be number");
 });
 test("One argument not number", () => {
   expect(divide([], 2)).toBe("All Parameters should be number");
@@ -23,4 +23,7 @@ test("divide 0 / 4 to equals 0", () => {
 });
 test("divide 4 / 0 to return denominator cannot be zero", () => {
   expect(divide(4, 0)).toBe("Denominator cannot be zero");
+});
+test("the case of null arguments", () => {
+  expect(divide(null, 5)).toBe("All Parameters should be number");
 });
