58354f54e52c8587fef315545ffdeca7fc8cedae
Izuchukwu Ogbodo <matthiasogbodo@gmail.com>
2019-04-06 13:04:01 +0100

    One argument not number

--- Addition/Add.test.js
+++ Addition/Add.test.js
@@ -7,3 +7,10 @@ test("adds 2 + 4 to equals 6", () => {
 test("Empty arguments ", () => {
   expect(add()).toEqual("No parameter should be empty");
 });
+
+test("One Empty argument ", () => {
+  expect(add(2)).toBe("No parameter should be empty");
+});
+test("One argument not number", () => {
+  expect(add([], 2)).toBe("All Parameters should be number");
+});
