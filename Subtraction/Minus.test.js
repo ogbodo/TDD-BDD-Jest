5b7ccd945fc2817a738bbc9ffc944130634e18cd
Izuchukwu Ogbodo <matthiasogbodo@gmail.com>
2019-04-06 18:04:16 +0100

    Empty arguments

--- Subtraction/Minus.test.js
+++ Subtraction/Minus.test.js
@@ -2,3 +2,6 @@ const minus = require("./minus");
 test("minus 4 - 2 to equals 2", () => {
   expect(minus(4, 2)).toBe(2);
 });
+test("Empty arguments ", () => {
+  expect(minus()).toEqual("No parameter should be empty");
+});
