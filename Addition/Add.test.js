83d09fc4045fabe5a240985b44373af59c55a4f0
Izuchukwu Ogbodo <matthiasogbodo@gmail.com>
2019-04-06 12:29:22 +0100

    Empty arguments

--- Addition/Add.test.js
+++ Addition/Add.test.js
@@ -3,3 +3,7 @@ const add = require("./Add");
 test("adds 2 + 4 to equals 6", () => {
   expect(add(2, 4)).toBe(6);
 });
+
+test("Empty arguments ", () => {
+  expect(add()).toEqual("No parameter should be empty");
+});
