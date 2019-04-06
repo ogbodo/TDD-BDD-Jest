0be1101fba4ac78b70e686669bba0feb975217a9
Izuchukwu Ogbodo <matthiasogbodo@gmail.com>
2019-04-06 19:03:11 +0100

    One Empty argument

--- StringConcat/Concat.test.js
+++ StringConcat/Concat.test.js
@@ -5,3 +5,6 @@ test("concatinate Izu and chukwu to equals Izuchukwu", () => {
 test("Empty arguments ", () => {
   expect(str()).toEqual("No parameter should be empty");
 });
+test("One Empty argument ", () => {
+  expect(str("Izu")).toBe("All Parameters should be number");
+});
