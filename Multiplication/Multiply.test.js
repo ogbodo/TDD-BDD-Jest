f57cd132114af81f1f7297f59f6c1be9da3596d6
Izuchukwu Ogbodo <matthiasogbodo@gmail.com>
2019-04-06 13:57:02 +0100

    One Empty argument

--- Multiplication/Multiply.test.js
+++ Multiplication/Multiply.test.js
@@ -6,3 +6,6 @@ test("Multiply 2 * 4 to equals 8", () => {
 test("Empty arguments ", () => {
   expect(multiply()).toEqual("No parameter should be empty");
 });
+test("One Empty argument ", () => {
+  expect(add(2)).toBe("No parameter should be empty");
+});
