1c8c0cd23fdb1075db29f59310e67d4361bbfd5f
Izuchukwu Ogbodo <matthiasogbodo@gmail.com>
2019-04-06 19:06:43 +0100

    Passed test => one Empty argument

--- StringConcat/concat.js
+++ StringConcat/concat.js
@@ -2,5 +2,10 @@ module.exports = function(str1, str2) {
   if (!str1 && !str2) {
     return "No parameter should be empty";
   }
+  if (!str1 || !str2) {
+    if (typeof str1 !== "string" || typeof str2 !== "string") {
+      return "All Parameters should be number";
+    }
+  }
   return str1 + str2;
 };
