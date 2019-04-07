d7619cca70e621f4281bca974d55af3b0a2f50ca
Izuchukwu Ogbodo <matthiasogbodo@gmail.com>
2019-04-07 17:45:04 +0100

    Passed test => Empty arguments

--- InfiniteMultiply/InfiniteTimes.js
+++ InfiniteMultiply/InfiniteTimes.js
@@ -1,4 +1,7 @@
 module.exports = function() {
+  if (arguments.length < 1) {
+    return " parameter should not be empty";
+  }
   var multiply = 1;
   for (var i = 0; i < arguments.length; i++) {
     multiply *= arguments[i];
