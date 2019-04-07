129774c881ff484b1109eebb53c70a4d13958049
Izuchukwu Ogbodo <matthiasogbodo@gmail.com>
2019-04-07 17:54:02 +0100

    Passed test => One argument is array of number

--- InfiniteMultiply/InfiniteTimes.js
+++ InfiniteMultiply/InfiniteTimes.js
@@ -4,7 +4,25 @@ module.exports = function() {
   }
   var multiply = 1;
   for (var i = 0; i < arguments.length; i++) {
-    multiply *= arguments[i];
+    if (isArray(arguments[i])) {
+      multiply *= multiplyArrayValues(arguments[i]);
+    } else multiply *= arguments[i];
   }
   return multiply;
 };
+function isArray(value) {
+  return value instanceof Array;
+}
+function multiplyArrayValues(arr) {
+  var multiply = 1;
+  for (var i = 0; i < arr.length; i++) {
+    if (isArray(arr[i])) {
+      multiply *= sumArrayValues(arr[i]);
+    } else if (typeof arr[i] === "object" && arr[i] !== null) {
+      multiply *= sumObjectValues(arr[i]);
+    } else {
+      multiply *= arr[i];
+    }
+  }
+  return multiply;
+}
