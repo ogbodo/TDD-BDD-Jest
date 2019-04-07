49e43c0c1e155c70dda2187323c89838bf416b74
Izuchukwu Ogbodo <matthiasogbodo@gmail.com>
2019-04-07 13:35:23 +0100

    Empty Argument

--- InfiniteAddition/InfiniteAddition.test.js
+++ InfiniteAddition/InfiniteAddition.test.js
@@ -2,3 +2,6 @@ const indefiniteAdd = require("./InfiniteAdd");
 test("adds 2 + 4 + 5 + 6 to equals 17", () => {
   expect(indefiniteAdd(2, 4, 5, 6)).toBe(17);
 });
+test("Empty arguments ", () => {
+  expect(indefiniteAdd()).toEqual("No parameter should be empty");
+});
