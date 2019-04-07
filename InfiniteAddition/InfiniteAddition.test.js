e4fdbe1be4deaef9513275e4f2537fe38424263c
Izuchukwu Ogbodo <matthiasogbodo@gmail.com>
2019-04-07 15:02:57 +0100

    One argument is array of number

--- InfiniteAddition/InfiniteAddition.test.js
+++ InfiniteAddition/InfiniteAddition.test.js
@@ -5,3 +5,9 @@ test("adds 2 + 4 + 5 + 6 to equals 17", () => {
 test("Empty arguments ", () => {
   expect(indefiniteAdd()).toEqual("No parameter should be empty");
 });
+test("One argument ", () => {
+  expect(indefiniteAdd(2)).toBe(2);
+});
+test("One argument is array of number", () => {
+  expect(indefiniteAdd([2, 3, 4], 2, 4, 9, 0)).toBe(24);
+});
