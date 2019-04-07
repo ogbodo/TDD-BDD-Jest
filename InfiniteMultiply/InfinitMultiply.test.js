5fa2bc093a994b87111928065d3b876d10191218
Izuchukwu Ogbodo <matthiasogbodo@gmail.com>
2019-04-07 17:47:17 +0100

    One argument is array of number

--- InfiniteMultiply/InfinitMultiply.test.js
+++ InfiniteMultiply/InfinitMultiply.test.js
@@ -5,3 +5,9 @@ test("Multiply 2 * 4 * 5 * 1 to equals 40", () => {
 test("Empty arguments ", () => {
   expect(indefiniteMultiply()).toEqual(" parameter should not be empty");
 });
+test("One argument ", () => {
+  expect(indefiniteMultiply(2)).toBe(2);
+});
+test("One argument is array of number", () => {
+  expect(indefiniteMultiply([2, 3, 1], 2, 1, 1, 2)).toBe(24);
+});
