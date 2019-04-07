e01410231970f677b88522cad0ce29caf15032c6
Izuchukwu Ogbodo <matthiasogbodo@gmail.com>
2019-04-07 13:36:09 +0100

    Passed test => Empty Argument

--- InfiniteAddition/InfiniteAdd.js
+++ InfiniteAddition/InfiniteAdd.js
@@ -1,4 +1,7 @@
 module.exports = function() {
+  if (arguments.length < 1) {
+    return "No parameter should be empty";
+  }
   var sum = 0;
   for (var i = 0; i < arguments.length; i++) {
     sum += arguments[i];
