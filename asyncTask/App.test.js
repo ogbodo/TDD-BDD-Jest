e2b5b1eb66fd52dae22a5be68a1606f5a2f58416
Izuchukwu Ogbodo <matthiasogbodo@gmail.com>
2019-04-11 16:19:04 +0100

    minor changes made

--- asyncTask/App.test.js
+++ asyncTask/App.test.js
@@ -2,7 +2,6 @@ const { getUserName } = require("./App");
 
 test("returns the user repo", () => {
   getUserName("ogbodo").then(function(repos) {
-    console.log(repos.length);
     expect(repos["owner"].login).toBe("ogbodo");
   });
 });
